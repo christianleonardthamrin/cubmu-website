b71b367969a12f98385f0ba3c03533740421f863
Christian Leonard <thamrchris@gmail.com>
2023-02-28 22:05:42 +0700

    add custom tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ module.exports = {
     letterSpacing: {
       navlink: '.04em'
     },
-    extend: {},
+    extend: {
+      spacing: {
+        '66px': '66px',
+        '97px': '97px',
+        '156px': '156px',
+      },
+      colors: {
+        'tab-active': '#D03138',
+        'tab-inactive': '#555657'
+      }
+    },
   },
   plugins: [],
 }
