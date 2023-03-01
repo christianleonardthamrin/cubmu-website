d6be4d3309ddaee0948a885fb3c26a9d731c0945
Christian Leonard <thamrchris@gmail.com>
2023-03-01 20:55:17 +0700

    fix build error

--- src/pages/components/main/components/card.js
+++ src/pages/components/main/components/card.js
@@ -12,23 +12,26 @@ export default function Card({selectCouponHandler, coupon, setIsTnc}) {
     return (
         <div className="card flex lg:flex-col rounded w-full h-150px lg:h-475px bg-white">
             <div className="image-wrapper">
-                <Image
-                    className='rounded-tl rounded-bl lg:rounded-t lg:rounded-bl-none w-150px h-150px lg:w-305px lg:h-305px'
-                    src={coupon.couponBrandLogo} 
-                    width={305}
-                    height={305}
-                    alt='spoon'
-                />
+                {coupon ? (
+                    <Image
+                        className='rounded-tl rounded-bl lg:rounded-t lg:rounded-bl-none w-150px h-150px lg:w-305px lg:h-305px'
+                        src={coupon.couponBrandLogo} 
+                        width={305}
+                        height={305}
+                        alt='spoon'
+                    />
+                ) : null }
+                
             </div>
             <div className="p-4 flex flex-col">
-                <h2 className="text-black text-base lg:text-xl font-bold">{coupon.couponName}</h2>
+                <h2 className="text-black text-base lg:text-xl font-bold">{coupon && coupon.couponName}</h2>
                 <div className="flex justify-between items-center">
                     <span className={`${inter.className} text-lg lg:text-2xl text-discount font-semibold flex items-center`}>
-                        {coupon.couponBenefitType === 'Cashback' ? (
+                        {coupon && coupon.couponBenefitType === 'Cashback' ? (
                             'Rp. '
                         ) : null}
-                        {coupon.couponBenefitValue}
-                        {coupon.couponBenefitType === 'Discount' ? (
+                        {coupon && coupon.couponBenefitValue}
+                        {coupon && coupon.couponBenefitType === 'Discount' ? (
                             <span className="text-off text-base font-medium ml-6px">Off</span>
                         ) : null}
                     </span>
@@ -40,7 +43,7 @@ export default function Card({selectCouponHandler, coupon, setIsTnc}) {
                         T&C {' >'}
                     </button>
                 </div>
-                <span className="text-10px lg:text-xs font-normal text-promo mt-2">Promo Sampai {getLocaleDateString(coupon.couponEndDate)}</span>
+                <span className="text-10px lg:text-xs font-normal text-promo mt-2">Promo Sampai {coupon ? getLocaleDateString(coupon.couponEndDate) : null}</span>
                 <button 
                     className="mt-2 bg-discount w-161px py-1 lg:py-2 lg:w-full text-xs lg:text-base font-bold self-center rounded"
                     onClick={() => selectCouponHandler(coupon)}
