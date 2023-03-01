1c1ffd7d42375600b517ad0490e4e5124eab2753
Christian Leonard <thamrchris@gmail.com>
2023-03-01 21:30:11 +0700

    fix next config --test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: 'cloudinary',
+    loader: 'custom',
     domains: ['servicebuss-dev.transvision.co.id'],
     path: 'https://servicebuss-dev.transvision.co.id:9443/cubmu/images/trinvi/coupon_brand'
   }
