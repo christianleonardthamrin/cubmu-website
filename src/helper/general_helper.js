fa834caafc1f85fd66ac7c6326b19174e754a0a6
Christian Leonard <thamrchris@gmail.com>
2023-03-01 17:55:51 +0700

    add date locale string helper

--- src/helper/general_helper.js
+++ src/helper/general_helper.js
@@ -1,5 +1,12 @@
-export default function getBaseUrl () {
+export function getBaseUrl () {
     const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
     return BASE_URL
+}
+
+export function getLocaleDateString(dateString) {
+    const date = new Date(dateString)
+    const options = { year: 'numeric', month: 'long', day: 'numeric' }
+
+    return date.toLocaleDateString('id-ID', options)
 }
