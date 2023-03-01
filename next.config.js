83ffd5659e1ac2d2893d4968523dd9dba5614f62
Christian Leonard <thamrchris@gmail.com>
2023-03-01 16:19:18 +0700

    registering domain name on fetch image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['servicebuss-dev.transvision.co.id']
+  }
 }
 
 module.exports = nextConfig
